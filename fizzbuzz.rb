e5de0be9a3210640ab35d058d9eb02bf520e50a3
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-20 23:58:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,13 @@
 def fizzbuzz(number)
   if number % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end 
+  end
   if number % 5==0
     "Buzz"
   end
   if number % 3 ==0 && number % 5 ==0
     return "FizzBuzz"
-  end  
+  end
 end
 
 
